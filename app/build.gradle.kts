745e4fe86f95e12e609aafb272e1991b55114af6
PRIYANSHU RAMI <ramipriyanshu2004@gmail.com>
2024-11-27 09:51:13 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.rpm24.prac_exam_042"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rpm24.prac_exam_042"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/rpm24/prac_exam_042/MainActivity.kt
+++ app/src/main/java/com/rpm24/prac_exam_042/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rpm24.prac_exam_042.ui.theme.Prac_exam_042Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prac_exam_042Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    EventEase(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +37,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun EventEase(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.backicon),
+                contentDescription = null
+            )
+            Text(
+                text = "EventEase",
+                modifier = Modifier.padding(start = 10.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.share),
+                contentDescription = null,
+                modifier = Modifier.padding(start = 180.dp)
+            )
+        }
+        Image(
+            painter = painterResource(R.drawable.event),
+            contentDescription = "Event Image",
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+        )
+        Row {
+            Text(
+                text = "Tech Conference 2024",
+                modifier = Modifier
+                    .padding(top = 16.dp) // Adjusts spacing from the image above
+                    .fillMaxWidth(), // Makes the text span the entire row width
+                fontSize = 20.sp // Optional styling
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Prac_exam_042Theme {
-        Greeting("Android")
+        EventEase()
     }
 }
